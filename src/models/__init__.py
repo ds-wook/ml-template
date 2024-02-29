400882c16016911b94265ad3c2cbaa65cc582980
ds-wook <leewook94@gmail.com>
2024-02-29 12:34:31 +0900

    Update!

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,3 @@
 from .base import BaseModel
-from .nn import EarlyStoppingCallback, TabNetTrainer
-from .tree import CatBoostTrainer, LGBMTrainer, XGBTrainer
+from .nn import TabNetTrainer
+from .tree import CatBoostTrainer, LightGBMTrainer, XGBoostTrainer
