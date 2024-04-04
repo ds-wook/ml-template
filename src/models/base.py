acdcf340866518f349f4b9b9aae4c654f54d62d7
ds-wook <leewook94@gmail.com>
2024-04-04 15:17:35 +0900

    Update!

--- src/models/base.py
+++ src/models/base.py
@@ -15,6 +15,7 @@ from omegaconf import DictConfig
 from pytorch_tabnet.tab_model import TabNetRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import KFold
+from tqdm import tqdm
 
 
 @dataclass
@@ -56,28 +57,29 @@ class BaseModel(ABC):
         models = {}
         kfold = KFold(n_splits=self.cfg.data.n_splits, shuffle=True, random_state=self.cfg.data.seed)
 
-        for fold, (train_idx, valid_idx) in enumerate(iterable=kfold.split(X=X), start=1):
-            X_train, X_valid = X.iloc[train_idx], X.iloc[valid_idx]
-            y_train, y_valid = y.iloc[train_idx], y.iloc[valid_idx]
+        with tqdm(kfold.split(X=X), total=self.cfg.data.n_splits, desc="CV", leave=False) as pbar:
+            for fold, (train_idx, valid_idx) in enumerate(iterable=pbar, start=1):
+                X_train, X_valid = X.iloc[train_idx], X.iloc[valid_idx]
+                y_train, y_valid = y.iloc[train_idx], y.iloc[valid_idx]
 
-            model = self.fit(X_train, y_train, X_valid, y_valid)
-            oof_preds[valid_idx] = (
-                model.predict(X_valid)
-                if isinstance(model, lgb.Booster)
-                else (
-                    model.predict(xgb.DMatrix(X_valid))
-                    if isinstance(model, xgb.Booster)
+                model = self.fit(X_train, y_train, X_valid, y_valid)
+                oof_preds[valid_idx] = (
+                    model.predict(X_valid)
+                    if isinstance(model, lgb.Booster)
                     else (
-                        model.predict(X_valid.to_numpy()).reshape(-1)
-                        if isinstance(model, TabNetRegressor)
-                        else model.predict(X_valid)
+                        model.predict(xgb.DMatrix(X_valid))
+                        if isinstance(model, xgb.Booster)
+                        else (
+                            model.predict(X_valid.to_numpy()).reshape(-1)
+                            if isinstance(model, TabNetRegressor)
+                            else model.predict(X_valid)
+                        )
                     )
                 )
-            )
-            models[f"fold_{fold}"] = model
+                models[f"fold_{fold}"] = model
 
-        del X_train, X_valid, y_train, y_valid
-        gc.collect()
+            del X_train, X_valid, y_train, y_valid
+            gc.collect()
 
         print(f"CV Score: {mean_absolute_error(y, oof_preds):.6f}")
 
